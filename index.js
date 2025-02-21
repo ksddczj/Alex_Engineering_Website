1a3e2cd873c7b77edd5ae46c3b5d0ec00bcfdba1
ksddczj <ksddczj@hotmail.com>
2025-02-21 23:34:32 +1100

    Changed sqlite3 to win32 version for azure deployment and other minor changes

--- index.js
+++ index.js
@@ -17,69 +17,69 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // // set up DB
-// let sqlite3 = require('sqlite3').verbose();
-// let db = new sqlite3.Database('.\\database\\myDB', sqlite3.OPEN_READWRITE, (err) => {
-//         if (err) {
-//             throw err.message;
-//         }else {
-//             console.log("DB connection successful.");
-//         }
-// }); 
+let sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('.\\database\\myDB', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            throw err.message;
+        }else {
+            console.log("DB connection successful.");
+        }
+}); 
 
 // ********************************************
 // *** Other route/request handlers go here ***
 // ********************************************`
 
-// app.post('/clientrequest', (req, res) => {
-//   const submittedData = req.body;
+app.post('/clientrequest', (req, res) => {
+  const submittedData = req.body;
 
-//   res.render('thankyou', { 
-//     submittedData: submittedData
-//   });
-//   // save submittedData to DB
-//   console.log("Client message received.")
+  res.render('thankyou', { 
+    submittedData: submittedData
+  });
+  // save submittedData to DB
+  console.log("Client message received.")
 
 
-//   let firstname = req.body.fname;
-//   let lastname = req.body.lname;
-//   let email = req.body.email;
-//   let mobile = req.body.mobile;
-//   let street = req.body.street;
-//   let city = req.body.city;
-//   let state = req.body.state;
-//   let servicetype = req.body.servicetype;
-//   let message = req.body.message;
+  let firstname = req.body.fname;
+  let lastname = req.body.lname;
+  let email = req.body.email;
+  let mobile = req.body.mobile;
+  let street = req.body.street;
+  let city = req.body.city;
+  let state = req.body.state;
+  let servicetype = req.body.servicetype;
+  let message = req.body.message;
 
-//   // console.log(`First Name: ${firstname}`);
-//   // console.log(`Last Name: ${lastname}`);
-//   // console.log(`Email: ${email}`);
-//   // console.log(`Mobile: ${mobile}`);
-//   // console.log(`Street: ${street}`);
-//   // console.log(`City: ${city}`);
-//   // console.log(`State: ${state}`);
-//   // console.log(`Service Type: ${servicetype}`);
-//   // console.log(`Message: ${message}`);
+  // console.log(`First Name: ${firstname}`);
+  // console.log(`Last Name: ${lastname}`);
+  // console.log(`Email: ${email}`);
+  // console.log(`Mobile: ${mobile}`);
+  // console.log(`Street: ${street}`);
+  // console.log(`City: ${city}`);
+  // console.log(`State: ${state}`);
+  // console.log(`Service Type: ${servicetype}`);
+  // console.log(`Message: ${message}`);
 
-//   db.run(
-//     `INSERT INTO Client (firstname, lastname, email, mobile, street, city, state, servicetype, message) VALUES ('${firstname}', '${lastname}', '${email}', '${mobile}', '${street}', '${city}', '${state}', '${servicetype}', '${message}')`,
-//     (err) => {
-//       if (err) {
-//         console.error(err.message);
-//       } else {
-//         console.log(`A row has been inserted to Table Client`);
-//       }
-//     }
-//   );
-// });
+  db.run(
+    `INSERT INTO Client (firstname, lastname, email, mobile, street, city, state, servicetype, message) VALUES ('${firstname}', '${lastname}', '${email}', '${mobile}', '${street}', '${city}', '${state}', '${servicetype}', '${message}')`,
+    (err) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log(`A row has been inserted to Table Client`);
+      }
+    }
+  );
+});
 
-// app.post("/searchprojects", (req, res) => {
-//   const searchTerm = req.body.projectnamekeyword;
-//   console.log(searchTerm);
-//   res.render("existingproject", {});
+app.post("/searchprojects", (req, res) => {
+  const searchTerm = req.body.projectnamekeyword;
+  console.log(searchTerm);
+  res.render("existingproject", {});
 
-//   // save submittedData to DB
-//   console.log("Search project requested received.");
-// });
+  // save submittedData to DB
+  console.log("Search project requested received.");
+});
 
 
 
